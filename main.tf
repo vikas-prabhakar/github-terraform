e4b3403c370b92636a224631ee2395c18b8d700b
Vikas Sharma <vikas>
2020-02-22 22:22:46 +0530

    region define

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ec2-github"
     key            = "terraform.tfstate"
+    region         = "ap-south-1"
     dynamodb_table = "ec2-guthub"
   }
 }
